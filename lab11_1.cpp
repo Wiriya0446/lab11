b34b3dcbfdd37d4fcc5eaf3c22a06338ce47b9df
Wiriya0446 <wiriyakhotchahin@gmail.com>
2023-12-26 13:32:03 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+using namespace std;
+string x[]={"A","B+","B","C+","C","D+","D","F","W"};
+int i=0,g;
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    while(i<3){
+        cin.get();
+        i++;
+    }
+    g=rand()%9;
+    cout << "You will get "<<x[g]<< " in this 261102.";
+    return 0;
+}
